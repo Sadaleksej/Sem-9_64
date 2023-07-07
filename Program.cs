d65365b5524082dd05bc1cd774d154d7ca4ec39d
Sadaleksej <135204154+Sadaleksej@users.noreply.github.com>
2023-07-07 14:23:22 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+using static System.Console;
+
+Clear();
+WriteLine();
+
+Console.WriteLine("Введите значение a: ");
+int a = Convert.ToInt32(ReadLine());
+Console.WriteLine("Введите значение b: ");
+int b = Convert.ToInt32(ReadLine());
+WriteLine(Numbers(a,b));
+
+
+
+string Numbers (int aa, int bb)
+{
+    if (aa >= bb) return $"{bb} " + Numbers(aa, bb+1); 
+    else return String.Empty;
+}
